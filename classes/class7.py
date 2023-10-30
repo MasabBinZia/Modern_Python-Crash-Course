d59ea9c5ca08fa87c0eff13eea49a8d0568c6dfb
Masab Bin Zia <masabmbz5@gmail.com>
2023-10-31 02:01:15 +0500

    class7

---
+++ classes/class7.py
@@ -0,0 +1,48 @@
+from typing import Dict,Union,Optional
+import pprint
+
+Key = Union[int,str]
+Value =Union[int,str,list,dict,tuple,set]
+
+
+data: Dict[Key, Value] = {
+    "fname": "Masab", 
+    "name": "Mbz", 
+    "lname": "Zia",
+    100:"Pakistan",
+    "abc":{1,2,3},
+    "xyz":[1,2,3],
+    "efg":(1,2,3),
+    "zxc":{"a":1,"b":2}
+    }
+
+# pprint.pprint(data)
+# print(data["fname"])
+# print(data["name"])
+# print(data["lname"])
+# print(data[100])
+# print(data["xyz"])
+
+# data["name"]= "Masab Bin Zia"
+# print(data["name"])
+# print(data.get["name","NA"])
+
+# print(data.keys())
+# print(data.values())
+# print(data.items())
+
+
+
+# for v in data.values():
+#     print(v)
+
+# for k,v in data.items():
+#     print(f"Key:{k}, Value:{v}")
+
+keys :list[str]=['id',"Name","FirstName","Course"]
+
+StdData:dict[keys,Value]={}
+
+print(StdData)
+StdData = StdData.fromkeys(keys)
+print(StdData)
